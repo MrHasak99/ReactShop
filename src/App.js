db3022899e73a7a1440d3c9fb7e62b1d56dafd4d
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-10 20:20:28 +0300

    correction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from "react";
 import shop from "./images/cookieshop.jpeg";
 import "./App.css";
 
 function App() {
   return (
     <body>
-      <h1 class="header">Cookies & Beyond</h1>
-      <h4 class="header">Where cookies maniacs gether</h4>
-      <img class="image" src={shop} alt="Cookie Shop"></img>
+      <h1 className="header">Cookies & Beyond</h1>
+      <h4 className="header">Where cookies maniacs gether</h4>
+      <img className="image" src={shop} alt="Cookie Shop"></img>
     </body>
   );
 }
