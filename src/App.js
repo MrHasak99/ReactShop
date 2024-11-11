034caee8e8f4ce2035c06f40cb4c6a84fef6a743
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-11 12:14:56 +0300

    task part 3 complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import shop from "./images/cookieshop.jpeg";
 import "./App.css";
-import {products} from "./products";
+import { products } from "./products";
 function App() {
   const product = [
     {
@@ -16,30 +16,27 @@ function App() {
       price: "10 KD",
     },
   ];
+  const productsList = product.map(function (product) {
+    return (
+      <div className="product">
+        <img
+          className="product-image"
+          src={product.image}
+          alt="Chocolate Chip Cookies"
+        ></img>
+        <h3>{product.name}</h3>
+        <h5>{product.price}</h5>
+      </div>
+    );
+  });
   return (
     <body>
       <h1 className="header">Cookies & Beyond</h1>
       <h4 className="header">Where cookies maniacs gether</h4>
       <img className="image" src={shop} alt="Cookie Shop"></img>
       <div className="product-list">
+        {productsList}
         <div className="product">
-          <img
-            className="product-image"
-            src={product[0].image}
-            alt="Chocolate Chip Cookies"
-          ></img>
-          <h3>{product[0].name}</h3>
-          <h5>{product[0].price}</h5>
-        </div>
-        <div className="product">
-          <img
-            className="product-image"
-            src={product[1].image}
-            alt="Cute Cookies"
-          ></img>
-          <h3>{product[1].name}</h3>
-          <h5>{product[1].price}</h5>
-        </div><div className="product">
           <img
             className="product-image"
             src={products[0].image}
@@ -47,7 +44,8 @@ function App() {
           ></img>
           <h3>{products[0].name}</h3>
           <h5>{products[0].price}</h5>
-        </div><div className="product">
+        </div>
+        <div className="product">
           <img
             className="product-image"
             src={products[1].image}
