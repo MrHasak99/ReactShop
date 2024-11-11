5ca62e3b308ed78be4b4969656b7b94cb85ac159
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-11 10:59:55 +0300

    task part 2 complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
 import shop from "./images/cookieshop.jpeg";
 import "./App.css";
-
+import {products} from "./products";
 function App() {
+  const product = [
+    {
+      name: "Chocolate Chip Cookies",
+      image:
+        "https://yogaofcooking.co/wp-content/uploads/2020/10/old-fashioned-chocolate-chip-cookies-2.jpg",
+      price: "5 KD",
+    },
+    {
+      name: "Cute Cookies",
+      image:
+        "https://img-global.cpcdn.com/recipes/4859182992850944/1360x964cq70/teddy-bear-cookies-recipe-main-photo.webp",
+      price: "10 KD",
+    },
+  ];
   return (
     <body>
       <h1 className="header">Cookies & Beyond</h1>
       <h4 className="header">Where cookies maniacs gether</h4>
       <img className="image" src={shop} alt="Cookie Shop"></img>
+      <div className="product-list">
+        <div className="product">
+          <img
+            className="product-image"
+            src={product[0].image}
+            alt="Chocolate Chip Cookies"
+          ></img>
+          <h3>{product[0].name}</h3>
+          <h5>{product[0].price}</h5>
+        </div>
+        <div className="product">
+          <img
+            className="product-image"
+            src={product[1].image}
+            alt="Cute Cookies"
+          ></img>
+          <h3>{product[1].name}</h3>
+          <h5>{product[1].price}</h5>
+        </div><div className="product">
+          <img
+            className="product-image"
+            src={products[0].image}
+            alt="Gingerbread Cookies"
+          ></img>
+          <h3>{products[0].name}</h3>
+          <h5>{products[0].price}</h5>
+        </div><div className="product">
+          <img
+            className="product-image"
+            src={products[1].image}
+            alt="Oatmeal Raisin Cookies"
+          ></img>
+          <h3>{products[1].name}</h3>
+          <h5>{products[1].price}</h5>
+        </div>
+      </div>
     </body>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,14 @@
+export const products = [
+  {
+    name: "Gingerbread Cookies",
+    image:
+      "https://www.onceuponachef.com/images/2023/12/gingerbread-cookies-1-1536x1025.jpg",
+    price: "7.5 KD",
+  },
+  {
+    name: "Oatmeal Raisin Cookies",
+    image:
+      "https://www.bakingyouhappier.com/wp-content/uploads/2024/03/IMG_5180-1365x2048.jpg",
+    price: "2.5 KD",
+  },
+];
